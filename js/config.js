aba1051ea22eeea4965c6ab3881f9c0f57c0ba20
bintarobodypaint-monitoring <bintarobodypaint@gmail.com>
2026-09-21 00:59:56 +0700

    Delete js/config.js

--- js/config.js
+++
@@ -1,7 +0,0 @@
-// CENTRAL CONFIG — endpoint values are copied from the supplied working HTML.
-// Do not change these URLs unless the corresponding Apps Script deployment changes.
-const API = Object.freeze({
-  JPCB: 'https://script.google.com/macros/s/AKfycbw5a2s4WxsPFTHfbXMI0DrZBiECdFcheOEbbwkFBY-9eypxT8ybe8lPMW--ALfDpcJY/exec',
-  REDO: 'https://script.google.com/macros/s/AKfycbzW_1V3ickfa1PVCEWtvem8PN8xTh9ho9nv-jKzogbIeL3n2slWwLzkjZuuPQgz2dII/exec',
-  QC: 'https://script.google.com/macros/s/AKfycbxX3UnuRwNcYWpGhyWsz-WVopHttb3tM5Qe381lLSdPR7gkeHjKJrExFxmByYem1Toz/exec'
-});
